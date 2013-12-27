def3302599a5ea625841b95e2ecaee95065472ff
Claire Huang <claire@govoluntr.com>
2013-12-26 18:26:38 -0800

    End of Chapter 3, switching to my computer again.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+	# returns the full title on a per-page basis.
+	# => if page doesn't specify title, then "Ruby on Rails Tutorial Sample App"
+	# => otherwise add a pipe to the end and add the specified title
+
+	def full_title(page_title)
+		base_title = "Ruby on Rails Tutorial Sample App"
+		if page_title.empty?
+			base_title    									# implicit return
+		else
+			"#{base_title} | #{page_title}}"
+		end
+	end
 end
